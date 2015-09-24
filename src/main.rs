e106053db06dc46782d72674b77150cee7c20f7c
jbranchaud <jbranchaud@gmail.com>
2015-09-23 20:22:32 -0500

    Extract path printing into separate function.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use std::env;
+use std::ffi::OsString;
+
+fn print_path(path_variable: OsString) {
+    let path_string: String = match path_variable.into_string() {
+        Ok(string) => string,
+        Err(_) => panic!("failure"),
+    };
+    let paths = path_string.split(":");
+    for path in paths {
+        println!("{}", path)
+    }
+}
 
 fn main() {
     let key = "PATH";
     match env::var_os(key) {
-        Some(val) => {
-            let path_variable: String = match val.into_string() {
-                Ok(string) => string,
-                Err(_) => panic!("failure"),
-            };
-            let paths = path_variable.split(":");
-            for path in paths {
-                println!("{}", path)
-            }
-        },
+        Some(val) => print_path(val),
         None => println!("{} is not defined in this environment", key)
     }
 }
